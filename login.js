563d109638abdf3c63bfcdd49896fbae23aed527
jwwitherspoon <jwwitherspoon@gmail.com>
2018-10-01 13:52:48 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+function login() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+	if ((username == "joel" && password == "joel") || (username == "nikki" && password == "nikki")) {
+		window.location.href = "https://jwwitherspoon.github.io/nikki"
+	}
+	else {
+		document.getElementById("message").innerHTML = "Incorrect login credentials."
+	}
+}
