8e761ab590ad94fc4703206951b583aa271e9a43
jwwitherspoon <jwwitherspoon@gmail.com>
2018-10-08 15:29:25 -0400

    Update card.js
    Added reasons 22-60.

--- card.js
+++ card.js
@@ -29,7 +29,7 @@ function previous() {
 
 function random() {
 	//Get a random card number between 1 and 365, inclusive
-	var cardNumber = Math.floor(Math.random() * 21) + 1;
+	var cardNumber = Math.floor(Math.random() * 365) + 1;
 	card(cardNumber);
 }
 
@@ -98,6 +98,123 @@ function card(cardNumber) {
 			break;
 		case 21:
 			document.getElementById("card").innerHTML = "21. We have the same decorating tastes.";
+			break;
+		case 22:
+			document.getElementById("card").innerHTML = "22. You support me in my decisions even when you don't agree with them.";
+			break;
+		case 23:
+			document.getElementById("card").innerHTML = "23. You are so good at playing piano!.";
+			break;
+		case 24:
+			document.getElementById("card").innerHTML = "24. You write songs with me.";
+			break;
+		case 25:
+			document.getElementById("card").innerHTML = "25. You play music with me.";
+			break;
+		case 26:
+			document.getElementById("card").innerHTML = "26. You give me words of affirmation even when you don't necessarily know how to.";
+			break;
+		case 27:
+			document.getElementById("card").innerHTML = "27. You make up weird lyrics to songs.";
+			break;
+		case 28:
+			document.getElementById("card").innerHTML = "28. You go on double dates with my friends.";
+			break;
+		case 29:
+			document.getElementById("card").innerHTML = "29. You push me to do things that make me uncomfortable.";
+			break;
+		case 30:
+			document.getElementById("card").innerHTML = "30. You are adventurous in the bedroom.";
+			break;
+		case 31:
+			document.getElementById("card").innerHTML = "31. You are a good mother to Cleo.";
+			break;
+		case 32:
+			document.getElementById("card").innerHTML = "32. You are going to be an amazing mother to Cleo.";
+			break;
+		case 33:
+			document.getElementById("card").innerHTML = "33. You are learning healthy ways to deal with your feelings.";
+			break;
+		case 34:
+			document.getElementById("card").innerHTML = "34. You go on dates with me even when you're tired.";
+			break;
+		case 35:
+			document.getElementById("card").innerHTML = "35. You make dinner for me.";
+			break;
+		case 36:
+			document.getElementById("card").innerHTML = "36. You like cooking with me.";
+			break;
+		case 37:
+			document.getElementById("card").innerHTML = "37. You are organized.";
+			break;
+		case 38:
+			document.getElementById("card").innerHTML = "38. You are good at planning.";
+			break;
+		case 39:
+			document.getElementById("card").innerHTML = "39. You are a saver. I need that in my life.";
+			break;
+		case 40:
+			document.getElementById("card").innerHTML = "40. You dress well. Everything you wear looks so good on you.";
+			break;
+		case 41:
+			document.getElementById("card").innerHTML = "41. We have so much in common.";
+			break;
+		case 42:
+			document.getElementById("card").innerHTML = "42. You like sex a lot. ;)";
+			break;
+		case 43:
+			document.getElementById("card").innerHTML = "43. You push me to push myself.";
+			break;
+		case 44:
+			document.getElementById("card").innerHTML = "44. You have amazing boobs. They're the perfect size!";
+			break;
+		case 45:
+			document.getElementById("card").innerHTML = "45. Your curly hair is so beautiful.";
+			break;
+		case 46:
+			document.getElementById("card").innerHTML = "46. You make weird faces.";
+			break;
+		case 47:
+			document.getElementById("card").innerHTML = "47. You tease me about my grammar mistakes.";
+			break;
+		case 48:
+			document.getElementById("card").innerHTML = "48. You point me back to Jesus when I need it.";
+			break;
+		case 49:
+			document.getElementById("card").innerHTML = "49. You keep me grounded when I freak out about things or have silly impulses.";
+			break;
+		case 50:
+			document.getElementById("card").innerHTML = "50. You believe in me.";
+			break;
+		case 51:
+			document.getElementById("card").innerHTML = "51. You did SO well at planning our wedding!";
+			break;
+		case 52:
+			document.getElementById("card").innerHTML = "52. You have been supportive of my recovery journey.";
+			break;
+		case 53:
+			document.getElementById("card").innerHTML = "53. You have put so much time and effort into overcoming your binge eating.";
+			break;
+		case 54:
+			document.getElementById("card").innerHTML = "54. You laugh at your own jokes.";
+			break;
+		case 55:
+			document.getElementById("card").innerHTML = "55. Your eyes are so pretty.";
+			break;
+		case 56:
+			document.getElementById("card").innerHTML = "56. You make dirty jokes with me.";
+			break;
+		case 57:
+			document.getElementById("card").innerHTML = "57. You don't mind me cursing now and then, but you keep me from doing it too much.";
+			break;
+		case 58:
+			document.getElementById("card").innerHTML = "58. You let me introduce you to new music.";
+			break;
+		case 59:
+			document.getElementById("card").innerHTML = "59. You enjoy cuddles as much as I do.";
+			break;
+		case 60:
+			document.getElementById("card").innerHTML = "60. You take an interest in the things I like even when you don't understand them.";
 			break;
 		default:
 			document.getElementById("card").innerHTML = "Reason " + cardNumber + " does not exist yet.";
